9e6bfd454ef90e25a21b4706e08bde4153d57939
Musfique55 <mdmusfiquepatwary@gmail.com>
2024-05-15 22:32:22 +0600

    vercel deployment complete

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const port = process.env.PORT || 3000;
 const cors = require('cors');
 require('dotenv').config();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  }));
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
