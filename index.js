a10cc6ac6b82abffbe52f6206fd1a19af62a5faa
Musfique55 <mdmusfiquepatwary@gmail.com>
2024-05-16 13:59:58 +0600

    added single product search through api feature

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors({
   }));
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lzevybe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -42,6 +42,13 @@ async function run() {
         const books = booksCollection.find();
         const result = await books.toArray();
         res.send(result);
+    })
+
+    app.get('/allbooks/:id', async(req,res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await booksCollection.findOne(query);
+      res.send(result); 
     })
 
     // Send a ping to confirm a successful connection
